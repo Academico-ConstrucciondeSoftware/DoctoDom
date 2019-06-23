23830de9aba38eedbe9d2478b9a92d01268f1390
AngelR24 <angel.ruizlajara@gmail.com>
2019-06-23 19:17:15 -0400

    AppoimentRule New Rule()

--- DoctoDom/Rules/UserRule.cs
+++ DoctoDom/Rules/UserRule.cs
@@ -88,6 +88,51 @@ namespace DoctoDom.Rules
             return dataTable;
         }
 
+        public User GetUser(string NickName, string password)
+        {
+            User userResult = new User();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    string query = "select Id,UserName,LastName,Cedula,Phone1,Phone2, ";
+                    query += "Birthdate,UserAddres,Email,Nickname,UserPassword,UserType,Specialties,ImagePath,CreatedDate from Users ";
+                    query += "where Nickname='" + NickName + "' and UserPassword='" + password + "'";
+                    SqlCommand sqlCommand = new SqlCommand(query, conn);
+                    sqlCommand.CommandType = CommandType.Text;
+                    conn.Open();
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                    while (sqlDataReader.Read())
+                    {
+                        userResult.Id = Convert.ToInt32(sqlDataReader["Id"].ToString());
+                        userResult.UserName = sqlDataReader["UserName"].ToString();
+                        userResult.LastName = sqlDataReader["LastName"].ToString();
+                        userResult.Cedula = sqlDataReader["Cedula"].ToString();
+                        userResult.Phone1 = sqlDataReader["Phone1"].ToString();
+                        userResult.Phone2 = sqlDataReader["Phone2"].ToString();
+                        userResult.Birthdate = Convert.ToDateTime(sqlDataReader["Birthdate"].ToString());
+                        userResult.UserAddres = sqlDataReader["UserAddres"].ToString();
+                        userResult.Email = sqlDataReader["Email"].ToString();
+                        userResult.Nickname = sqlDataReader["Nickname"].ToString();
+                        userResult.UserPassword = sqlDataReader["UserPassword"].ToString();
+                        userResult.UserType = Convert.ToInt32(sqlDataReader["UserType"].ToString());
+                        userResult.Specialties = Convert.ToInt32(sqlDataReader["Specialties"].ToString());
+                        userResult.ImagePath = sqlDataReader["ImagePath"].ToString();
+                        userResult.CreatedDate = Convert.ToDateTime(sqlDataReader["CreatedDate"].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                return null;
+            }
+
+            return userResult;
+        }
+
+
     }
 }
 
